8274997ffd20060e0ae43922e8dc8de935f1751d
vkk1 <vkkadari@gmail.com>
2025-06-25 09:56:41 -0400

    day 2 complete

---
+++ agents/meal_planner_agent.py
@@ -0,0 +1,65 @@
+import os 
+import requests 
+import random 
+from dotenv import load_dotenv
+from models.state import CaloriePlanState
+
+load_dotenv() 
+API_KEY = os.getenv("SPOONACULAR_API_KEY")
+
+
+def fetch_meals(diet, target_calories, restrictions, count):
+    url = "https://api.spoonacular.com/recipes/complexSearch"
+    params = {
+        "apiKey": API_KEY, 
+        "diet": diet,
+        "number": 10, 
+        "maxCalories": target_calories, 
+        "addRecipeInformation": True
+    }
+
+    if restrictions: 
+        params["intolerances"] = ",".join(restrictions)
+
+    response = requests.get(url, params = params)
+
+    data = response.json()
+
+    return [r for r in data.get("results", []) if r.get("title")]
+
+def meal_planner_agent(state: CaloriePlanState) -> CaloriePlanState:
+    days = {}
+    meals_per_day = state.prefs.meals_per_day
+    total_days = state.prefs.days
+    per_meal_cal = state.prefs.calories_per_day
+
+    for d in range(total_days):
+        day_name = f"Day {d+1}"
+
+        meal_list = fetch_meals(
+            diet = state.prefs.diet,
+            target_calories = per_meal_cal,
+            restrictions = state.prefs.restrictions,
+            count = meals_per_day
+        )
+
+        if len(meal_list) < meals_per_day:
+            # use what we have
+            print(f"Warning: only found {len(meal_list)} meals for Day {d+1}")
+            chosen = meal_list  
+
+        else:
+            chosen = random.sample(meal_list, meals_per_day)
+
+        meal_labels = ["Breakfast", "Lunch", "Dinner"]
+        days[day_name] = {
+            label: meal["title"]
+            for label, meal in zip(meal_labels, chosen)
+        }
+
+
+    state.planned_meals = days
+    return state
+
+
+
--- graphs/calorie_plan_graph.py
+++ graphs/calorie_plan_graph.py
@@ -2,16 +2,21 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 from langgraph.graph import StateGraph
 from agents.input_agent import input_agent
 from models.state import CaloriePlanState
 from models.state import UserPrefs
+from agents.meal_planner_agent import meal_planner_agent
 
 # minimal langgraph test 
 graph = StateGraph(CaloriePlanState)
+
 graph.add_node("input", input_agent)
+graph.add_node("planner", meal_planner_agent)
+
 graph.set_entry_point("input")
+graph.add_edge("input", "planner")
+
 builder = graph.compile()
 
 if __name__ == "__main__":
@@ -20,7 +25,7 @@ if __name__ == "__main__":
         "meals_per_day": 3, 
         "days": 2, 
         "diet": "vegetarian", 
-        "restrictions": ["no mushrooms"]
+        "restrictions": []
     }
 
     prefs = UserPrefs(**sample_input)
